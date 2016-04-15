b76cd9fa9404ed68fc6f3cc5e1be20b5b605df1c
wildcountryboy0 <afsfrichards@gmail.com>
2016-04-15 13:21:53 -0400

    Added files via upload

---
+++ lab6-4.ps1
@@ -0,0 +1,23 @@
+﻿param (
+    [string]$path,
+    [string]$type
+)
+
+if ($type -eq "consultant"){
+    New-Item -Path C:/$path -name Projects -ItemType Directory 
+    New-Item -Path C:/$path/Projects -name Assets -ItemType Directory
+    New-Item -Path C:/$path/Projects -name Proposals -ItemType Directory
+    New-Item -Path C:/$path/Projects -name Reports -ItemType Directory
+    Copy-Item C:\allowed-asset-types.txt C:\$path\Projects\Assets
+}
+else{
+    New-Item -Path C:/$path -name Projects -ItemType Directory 
+    New-Item -Path C:/$path/Projects -name Budgets -ItemType Directory
+    New-Item -Path C:/$path/Projects -name Reports -ItemType Directory
+    $count = 1
+    While ($type -ne "consultant"){
+        for ($count    )
+}
+}
+
+w
